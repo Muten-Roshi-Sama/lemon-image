2bc98e8a61b74312a82d0e58e0e3cd86e3c01b6a
vass <vassvision@protonmail.com>
2026-04-09 14:52:28 +0200

    Pocket Monster and added ?gitignore files.

--- kernel-build/1_embedded_LKM/.modules.order.cmd
+++
@@ -1,1 +0,0 @@
-savedcmd_/home/vass/dev/OS/citro/1_embedded_LKM/modules.order := {   echo /home/vass/dev/OS/citro/1_embedded_LKM/hello.o; :; } > /home/vass/dev/OS/citro/1_embedded_LKM/modules.order
--- kernel-build/1_embedded_LKM/modules.order
+++
@@ -1,1 +0,0 @@
-/home/vass/dev/OS/citro/1_embedded_LKM/hello.o
